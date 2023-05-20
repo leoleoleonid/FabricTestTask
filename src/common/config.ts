bc89a3673e70daa098eaa17ba01b6180600acba8
leoleoleonid <leonidkoss1@gmail.com>
2023-05-20 21:45:33 +0300

    add config

---
+++ src/common/config.ts
@@ -0,0 +1,27 @@
+import { config as dotenvConfig } from "dotenv";
+if (process.env.NODE_ENV !== "test") {
+    dotenvConfig();
+}
+
+const getRequiredEnvVar = (name: string) => {
+    const value = process.env[name];
+    if (!value) {
+        console.error(`Missing required environment variable ${name}`);
+        process.exit(1);
+    }
+    return value;
+};
+
+export const config = {
+    db: {
+        host: getRequiredEnvVar("POSTGRES_HOST"),
+        user: getRequiredEnvVar("POSTGRES_USER"),
+        port: getRequiredEnvVar("POSTGRES_PORT"),
+        password: getRequiredEnvVar("POSTGRES_PASSWORD"),
+        database: getRequiredEnvVar("POSTGRES_DB"),
+    },
+    port: parseInt(process.env.PORT || "8000", 10),
+
+};
+
+export type ConfigType = typeof config;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express, { Application } from "express";
+import morgan from "morgan";
+import {config} from "./common/config";
 
-const PORT = process.env.PORT || 8000;
+const PORT = config.port;
 
 const app: Application = express();
 
+app.use(express.json());
+app.use(morgan("tiny"));
 app.get("/ping", async (_req, res) => {
     res.send({
         message: "pong",
