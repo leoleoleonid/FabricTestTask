d3b301e5fe191dc25016048e9674ed643f1d0238
leoleoleonid <leonidkoss1@gmail.com>
2023-05-21 02:14:58 +0300

    fix

--- src/common/db/connection.ts
+++ src/common/db/connection.ts
@@ -4,15 +4,3 @@ import {DataSourceOptions} from "typeorm/data-source/DataSourceOptions";
 
 const dbConfig = {...config.db};
 export const dataSource = new DataSource(dbConfig as DataSourceOptions);
-
-//     {
-//     type: config.db.type,
-//     host: config.db.host,
-//     port: config.db.port,
-//     username: config.db.username,
-//     password: config.db.password,
-//     database: config.db.database,
-//     entities: ["src/entity/*.js"],
-//     logging: true,
-//     synchronize: true,
-// })
